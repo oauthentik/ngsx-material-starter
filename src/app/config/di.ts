68d75883da16eed1736e1e972bcef466191e43ee
othmane elalaoui banouzi <othmaneabanouzi@gmail.com>
2020-03-20 10:24:51 +0100

    added styles

---
+++ src/app/config/di.ts
@@ -0,0 +1,8 @@
+import { InjectionToken } from "@angular/core";
+
+export const API: InjectionToken<string> = new InjectionToken("APP_API");
+export const AVATAR: InjectionToken<string> = new InjectionToken("APP_AVATAR");
+export const APP_LOGO: InjectionToken<string> = new InjectionToken("APP_LOGO");
+export const APP_IMAGES: InjectionToken<string> = new InjectionToken(
+  "APP_IMAGES"
+);
