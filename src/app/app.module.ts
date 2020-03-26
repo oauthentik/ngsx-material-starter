377f73c53365162c5894e09cf4aa94f2ec1302ca
othmane elalaoui banouzi <lateg15@gmail.com>
2020-03-26 12:13:28 +0100

    added font awesome angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { CoreModule } from "./core/core.module";
@@ -11,6 +11,7 @@ import { CoreModule } from "./core/core.module";
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    FontAwesomeModule,
     AppRoutingModule,
     CoreModule
   ],
