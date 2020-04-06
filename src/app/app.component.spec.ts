1db75d934bcb3d7d58911ec11123be100ed75090
othmane elalaoui banouzi <lateg15@gmail.com>
2020-04-06 12:56:27 +0100

    test(app.component): update test cases

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,8 @@
 import { TestBed, async } from "@angular/core/testing";
 import { RouterTestingModule } from "@angular/router/testing";
 import { AppComponent } from "./app.component";
+import { By } from "@angular/platform-browser";
+import { RouterOutlet } from "@angular/router";
 
 describe("AppComponent", () => {
   beforeEach(async(() => {
@@ -16,18 +18,8 @@ describe("AppComponent", () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ngsx-material-starte'`, () => {
+  it(`should contains a router-outlet`, () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual("ngsx-material-starte");
-  });
-
-  it("should render title in a h1 tag", () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector("h1").textContent).toContain(
-      "Welcome to ngsx-material-starte!"
-    );
+    expect(fixture.debugElement.query(By.directive(RouterOutlet))).toBeTruthy();
   });
 });
