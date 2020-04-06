e4955ebac9c08533db41f09f3dad5878c796db86
othmane elalaoui banouzi <lateg15@gmail.com>
2020-04-06 12:17:59 +0100

    test(safe.pipe): inject dependacy

--- src/app/shared/pipes/safe.pipe.spec.ts
+++ src/app/shared/pipes/safe.pipe.spec.ts
@@ -1,11 +1,12 @@
 /* tslint:disable:no-unused-variable */
 
-import { TestBed, async } from '@angular/core/testing';
-import { SafePipe } from './safe.pipe';
+import { TestBed } from "@angular/core/testing";
+import { DomSanitizer } from "@angular/platform-browser";
+import { SafePipe } from "./safe.pipe";
 
-describe('Pipe: Safee', () => {
-  it('create an instance', () => {
-    let pipe = new SafePipe();
+describe("Pipe: Safee", () => {
+  it("create an instance", () => {
+    const pipe = new SafePipe(TestBed.get(DomSanitizer));
     expect(pipe).toBeTruthy();
   });
 });
